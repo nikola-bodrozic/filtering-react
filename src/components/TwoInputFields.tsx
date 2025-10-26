0f6c0eebdfbdf812393ff7eb9d0c1e02d65248e8
Nikola <nikola@example.com>
2025-10-26 17:31:33 +0100

    fixed state issue

--- src/components/TwoInputFields.tsx
+++ src/components/TwoInputFields.tsx
@@ -4,20 +4,17 @@ import { cars, type Car } from './values';
 const TwoInputFields = () => {
     const [company, setCompany] = useState('');
     const [model, setModel] = useState('');
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        if (e.target.name === 'company') setCompany(e.target.value);
-        if (e.target.name === 'model') setModel(e.target.value);
-    };
+ 
     let filtered: Car[] = [...cars];
     filtered = filtered.filter((item: Car) => 
         item.company.toLowerCase().includes(company.toLowerCase()) 
-        && item.model.toLowerCase().includes(model.toLowerCase()))
+        && item.model.toLowerCase().includes(model.toLowerCase())
+    )
 
     return (
         <div style={{ border: "3px dotted black", padding: "1em" }}>
-            <input type="text" name="company" value={company} onChange={handleChange} placeholder="Company" />
-            <input type="text" name="model" value={model} onChange={handleChange} placeholder="Model" />
+            <input type="text" name="company" value={company} onChange={(e)=>setCompany(e.target.value)} placeholder="Company" />
+            <input type="text" name="model" value={model} onChange={(e)=>setModel(e.target.value)} placeholder="Model" />
             <ul>
                 {filtered.map((car) => (
                     <li key={car.id}>
